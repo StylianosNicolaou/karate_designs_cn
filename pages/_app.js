5006421794be736cc9f10ea79304bf950c1243d1
StylianosNicolaou <nicolaou.s24@live.unic.ac.cy>
2025-04-17 18:13:42 +0300

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import Loader from "../components/Loader";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Loader />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
