da54c1d12adcfa669e6c86adb64f58deac0136d9
StylianosNicolaou <nicolaou.s24@live.unic.ac.cy>
2025-04-19 21:17:46 +0300

    fixed sizes

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
       <Head>
         <title>Karate Designs CN | Design with Discipline</title>
         <link rel="icon" href="/favicon-64x64.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
           content="Karate Designs CN is a creative studio offering premium branding, website, and design services inspired by discipline and precision."
